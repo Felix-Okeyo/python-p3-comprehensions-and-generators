1bb622ee6892b2cd28ecb9779372fe70e8d83f46
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-16 19:39:49 +0300

    comments

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-#eturns a list of all of the even elements of a sequence of integers between 0 and 9
+#returns a list of all of the even elements of a sequence of integers between 0 and 9
 def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0 in range(0,10)]
     
-
+#returns a sentence list that had '!' appended to each element
 def make_exclamation(sentence_list):
     return [sentence + "!" for sentence in sentence_list]
